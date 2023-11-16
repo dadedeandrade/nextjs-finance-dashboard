d39e90ea2eeedbda70f7ff6badc34cd4ad22b3dc
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-11-16 20:11:40 -0300

    Fix/ Update file system architecture

---
+++ src/app/dashboard/DashboardContent.tsx
@@ -0,0 +1,11 @@
+import * as Mui from "@mui/material";
+
+function DashboardContent(props: any) {
+  return (
+    <Mui.Stack>
+      <h1>teste</h1>
+    </Mui.Stack>
+  );
+}
+
+export default DashboardContent;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,8 +24,18 @@ import React, { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import LogoutIcon from "@mui/icons-material/Logout";
+import DashboardContent from "./DashboardContent";
 
 function Dashboard() {
+  /*
+"date": Data da transação no formato EPOCH em milissegundos;
+"amount": Valor da transação em string, sem separador de decimais: A string "5565" representa uma transação de 55,65;
+"transaction_type": Representa se a transação foi uma receita (deposit) ou despesa (withdraw);
+"currency": Moeda da transação;
+"account": Empresa de origem ou destino da transação;
+"industry": Categoria de Indústria à qual a Empresa da transação pertence;
+"state": Estado onde a Empresa da transação está incorporada.
+*/
   const session = useSession();
 
   if (session.status == "unauthenticated") {
@@ -133,33 +143,7 @@ function Dashboard() {
               </List>
             </Stack>
           </Drawer>
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac. Consequat mauris nunc
-            congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque
-            volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-            tellus. Purus sit amet volutpat consequat mauris. Elementum eu
-            facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus
-            viverra accumsan in. In hendrerit gravida rutrum quisque non tellus
-            orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant morbi
-            tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend.
-            Commodo viverra maecenas accumsan lacus vel facilisis. Nulla posuere
-            sollicitudin aliquam ultrices sagittis orci a.
-          </Box>
+          <DashboardContent />
         </>
       )}
     </>
